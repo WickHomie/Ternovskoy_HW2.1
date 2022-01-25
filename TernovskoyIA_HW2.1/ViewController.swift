a0c3b45164bbf1f7fb451cf2b978b6a46587ba6d
wickapp <wickhomie@gmail.com>
2022-01-25 11:35:16 +0300

    project

--- TernovskoyIA_HW2.1/ViewController.swift
+++ TernovskoyIA_HW2.1/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    enum AllLights {
+        case red, yellow, green
     }
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.3
+    var allLights = AllLights.red
+    
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightOff
+        yellowLight.alpha = lightOff
+        greenLight.alpha = lightOff
+       }
+    
+    
+
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch allLights {
+        case .red:
+            greenLight.alpha = lightOff
+            redLight.alpha = lightOn
+            allLights = .yellow
+        case .yellow:
+            redLight.alpha = lightOff
+            yellowLight.alpha = lightOn
+            allLights = .green
+        case .green:
+            yellowLight.alpha = lightOff
+            greenLight.alpha = lightOn
+            allLights = .red
+        }
+    
+    }
+    
 }
-
